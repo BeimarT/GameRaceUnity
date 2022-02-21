3d3d1d8b728135779daecf61525e299d8a9a39e4
Marclegend <43781038+Marclegend@users.noreply.github.com>
2022-02-21 16:52:29 +0100

    Resume etc

--- Assets/Scripts/PlayMainMenu.cs
+++ Assets/Scripts/PlayMainMenu.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class PlayMainMenu : MonoBehaviour
 {
+    public GameObject pauseMenuUI;
+    public static bool GameIsPaused = false;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
