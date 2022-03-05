606cc8c7ee04d2d6d2f06a8866674e28b3ac0502
Beim2 <beimartocr@gmail.com>
2022-03-05 14:33:50 +0100

    intento de api call
    El script es RegisterBBDD

---
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/RegisterBBDD.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class RegisterBBDD : MonoBehaviour
+{
+    public InputField usernameField;
+    public InputField passwordField;
+    public Button submitButton;
+
+    public void Start()
+    {
+        StartCoroutine(Register());
+    }
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", "hola");
+        form.AddField("email", "hola");
+        form.AddField("name", "hola");
+        form.AddField("surname", "hola");
+        form.AddField("password", "hola");
+        form.AddField("rol", "hola");
+        using (UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/api/user", form))
+        {
+            yield return www.SendWebRequest();
+
+            if(www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            } else {
+                Debug.Log("Register" + www.downloadHandler.text);
+            }
+        }
+    }
+}
--- Assets/PlayMainMenu.cs
+++ Assets/PlayMainMenu.cs
@@ -9,4 +9,8 @@ public class PlayMainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
