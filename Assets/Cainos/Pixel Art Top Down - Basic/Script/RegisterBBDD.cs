8cc7acb24aa34349c84002c9ed4d02eefb88c1ab
Beim2 <beimartocr@gmail.com>
2022-03-07 15:30:43 +0100

    register script a medias

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/RegisterBBDD.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/RegisterBBDD.cs
@@ -9,6 +9,8 @@ public class RegisterBBDD : MonoBehaviour
     public InputField passwordField;
     public Button submitButton;
 
+    
+
     public void Start()
     {
         StartCoroutine(Register());
@@ -16,14 +18,11 @@ public class RegisterBBDD : MonoBehaviour
     IEnumerator Register()
     {
         WWWForm form = new WWWForm();
-        form.AddField("username", "hola");
-        form.AddField("email", "hola");
-        form.AddField("name", "hola");
-        form.AddField("surname", "hola");
-        form.AddField("password", "hola");
-        form.AddField("rol", "hola");
-        using (UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/api/user", form))
-        {
+        form.AddField("username", usernameField.text);
+        form.AddField("password", passwordField.text );
+        UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/api/user" , form);
+        
+        
             yield return www.SendWebRequest();
 
             if(www.result != UnityWebRequest.Result.Success)
@@ -33,5 +32,8 @@ public class RegisterBBDD : MonoBehaviour
                 Debug.Log("Register" + www.downloadHandler.text);
             }
         }
-    }
+        public void VerifyInputs()
+        {
+            submitButton.interactable = (usernameField.text.Length >= 8 && passwordField.text.Length >= 8);
+        }
 }
