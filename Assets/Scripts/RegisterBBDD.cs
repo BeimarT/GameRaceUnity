34e5d9fac46792f880b1cf1beca577d2a0948e38
Beim2 <beimartocr@gmail.com>
2022-03-08 16:01:14 +0100

    Script acabado de register

--- Assets/Scripts/RegisterBBDD.cs
+++ Assets/Scripts/RegisterBBDD.cs
@@ -18,11 +18,17 @@ public class RegisterBBDD : MonoBehaviour
     IEnumerator Register()
     {
         WWWForm form = new WWWForm();
-        form.AddField("username", usernameField.text);
-        form.AddField("password", passwordField.text );
-        UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/api/user" , form);
-        
-        
+        form.AddField("username", "Bei");
+        form.AddField("email", "Beimar@gmail.com");
+        form.AddField("name", "Beimar");
+        form.AddField("surname", "Torrez");
+        form.AddField("rol", "User");
+        form.AddField("password", "1234567");
+        form.AddField("detail", "nada");
+        form.AddField("otherInformation", "pruebas");
+
+        using(UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/api/user" , form))
+        { 
             yield return www.SendWebRequest();
 
             if(www.result != UnityWebRequest.Result.Success)
@@ -32,6 +38,8 @@ public class RegisterBBDD : MonoBehaviour
                 Debug.Log("Register" + www.downloadHandler.text);
             }
         }
+    }
+
         public void VerifyInputs()
         {
             submitButton.interactable = (usernameField.text.Length >= 8 && passwordField.text.Length >= 8);
