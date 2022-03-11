5c86586c5757b626b9d7805a6ac4005df940c867
Marclegend <43781038+Marclegend@users.noreply.github.com>
2022-03-11 18:57:19 +0100

    Revert "Rollbacks-Finished"
    This reverts commit f00de4e4d12607a8c1057c308c234c11f11694ad.

--- Assets/Scripts/PlayMainMenu.cs
+++ Assets/Scripts/PlayMainMenu.cs
@@ -10,34 +10,13 @@ public class PlayMainMenu : MonoBehaviour
     public GameObject pauseMenuUI;
     public static bool GameIsPaused = false;
 
-    public void ContraReloj()
+    public void PlayGame()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 1;
     }
-    public void MenuPerder()
-    {
-        SceneManager.LoadScene(6);
-    }
-    public void MenuVictoria()
-    {
-        SceneManager.LoadScene(7);
-    }
-    public void MenuLoginRegistro()
-    {
-        SceneManager.LoadScene(0);
-    }
-    public void Registro()
-    {
-        SceneManager.LoadScene(1);
-    }
-    public void LogIn()
-    {
-        SceneManager.LoadScene(2);
-    }
-    public void MainMenu()
-    {
-        SceneManager.LoadScene(3);
+    public void PlayGame1(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
     public void ExitGame()
     {
