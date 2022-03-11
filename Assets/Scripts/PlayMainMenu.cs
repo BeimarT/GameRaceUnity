5c04d2a68ab027513111d238471ad5b9a3995e9e
Marclegend <43781038+Marclegend@users.noreply.github.com>
2022-03-11 18:10:01 +0100

    Cambios varios en mapas

--- Assets/Scripts/PlayMainMenu.cs
+++ Assets/Scripts/PlayMainMenu.cs
@@ -10,10 +10,34 @@ public class PlayMainMenu : MonoBehaviour
     public GameObject pauseMenuUI;
     public static bool GameIsPaused = false;
 
-    public void PlayGame()
+    public void ContraReloj()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(4);
         Time.timeScale = 1;
+    }
+    public void MenuPerder()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void MenuVictoria()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void MenuLoginRegistro()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Registro()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LogIn()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(3);
     }
     public void ExitGame()
     {
